77b00119f8fd15704f02fd2b75fba8230458465f
Luis García Sánchez <luis.garciasanchez@unir.net>
2024-12-23 15:10:41 +0100

    feat(base): Ficheros de base del sitio

---
+++ crea_estructura.sh
@@ -0,0 +1,16 @@
+
+
+
+
+mkdir assets
+mkdir assets/css
+mkdir assets/js
+mkdir assets/images
+mkdir assets/media
+
+
+curl https://unirfp.tionazo.com/base/index.html > index.html
+curl https://unirfp.tionazo.com/base/assets/css/style.css > assets/css/style.css
+curl https://unirfp.tionazo.com/base/favicon.ico > favicon.ico
+curl https://unirfp.tionazo.com/base/assets/images/logo.webp > assets/images/logo.webp
+
